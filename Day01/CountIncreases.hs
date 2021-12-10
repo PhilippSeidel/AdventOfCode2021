c18b43d74df034048d974ffab0bfabb44ffa3270
Philipp Seidel <pues@localhost.localdomain>
2021-12-10 22:20:39 +0100

    second try

--- Day01/CountIncreases.hs
+++ Day01/CountIncreases.hs
@@ -4,11 +4,12 @@ import System.IO
 main = do
         handle <- openFile "input" ReadMode
         content <- hGetContents handle
-        putStr $ show $ countIncreases 0 $ map (read::String->Int) $ lines content
+        print $ countIncreases 0 $ map (read::String->Int) $ lines content
         hClose handle
 
 countIncreases :: Int -> [Int] -> Int 
-countIncreases acc (x:xs)
-                | x < head xs = countIncreases (acc + 1) xs
-                | otherwise = countIncreases acc xs
-countIncreases acc _ = acc
+countIncreases acc [] = acc
+countIncreases acc [x] = acc
+countIncreases acc (x:rest@(y:xs))
+                | x < y = countIncreases (acc + 1) rest
+                | otherwise = countIncreases acc rest
