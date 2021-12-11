a53ea944e0e894a5b5c9bd9b2cf0b89488d532b1
Philipp Seidel <pues@localhost.localdomain>
2021-12-11 19:50:09 +0100

    first try at day03 challange

---
+++ Day03/PowerConsumption.hs
@@ -0,0 +1,31 @@
+module Day03.PowerConsumption where
+import System.IO
+import Data.Char (digitToInt)
+import Text.Html (input)
+
+main = do
+        handle <- openFile "input" ReadMode
+        content <- hGetContents handle
+        print $ powerConsumption $ parseContent $ lines content
+        hClose handle
+
+parseContent :: [String] -> [[Int]]
+parseContent = map (map ((\x -> if x == 0 then -1 else x) . digitToInt))
+
+powerConsumption :: [[Int]] -> Int
+powerConsumption input = gamma input * epsilon input
+
+gamma :: [[Int]] -> Int
+gamma input = binaryToInt (mostComman (commonalityDistribution input))
+            where mostComman = map (\x -> if x < 0 then 0 else 1)
+
+epsilon :: [[Int]] -> Int
+epsilon input = binaryToInt (leastComman (commonalityDistribution input))
+            where leastComman = map (\x -> if x < 0 then 1 else 0)
+
+commonalityDistribution :: [[Int]] -> [Int]
+commonalityDistribution = foldr (zipWith (+)) [0..]
+
+binaryToInt :: [Int] -> Int
+binaryToInt [] = 0
+binaryToInt binary@(x:xs) = x * 2 ^ length binary + binaryToInt xs
