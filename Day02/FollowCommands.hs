8986f858d36fba4f832f4745e555ab9ef009ff41
Philipp Seidel <pues@localhost.localdomain>
2021-12-11 17:57:16 +0100

    first try at day02 challenge

---
+++ Day02/FollowCommands.hs
@@ -0,0 +1,21 @@
+module Day02.FollowCommands where
+import System.IO
+
+
+
+main = do
+        handle <- openFile "input" ReadMode
+        content <- hGetContents handle
+        print $ followCommands 0 0 $ concatMap words $ lines content
+        hClose handle
+
+followCommands :: Int -> Int -> [String] -> Int
+followCommands depth horizontal [] = depth * horizontal
+followCommands depth horizontal (command:parameter:rest)
+            | command == "forward" = followCommands depth (horizontal + parseToInt parameter) rest
+            | command == "up" = followCommands (depth + parseToInt parameter) horizontal rest
+            | command == "down" = followCommands (depth - parseToInt parameter) horizontal rest
+            | otherwise = error "unknown command"
+followCommands _ _ [x] = error "unequal number of commands and parameters"
+
+parseToInt = read::String->Int
